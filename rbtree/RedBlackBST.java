a513e94b40fff24df283922a1e2673182811c820
vitor <88738275+vitorindio@users.noreply.github.com>
2022-12-25 18:12:45 -0300

    rb tree with insetion method + auxiliares methods

---
+++ rbtree/RedBlackBST.java
@@ -0,0 +1,441 @@
+
+// RedBlack-Tree search, add, remove
+
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+
+    private static final boolean RED   = true;
+    private static final boolean BLACK = false;
+
+    private Node root;     // root of the BST
+
+   //  BST helper node data type
+    private class Node {
+        private Key key;           // chave/key
+        private Value val;         // valor/data
+        private Node left, right;  // filhos esquerdo e direito
+        private boolean color;     // cor do link que vem do pai!!
+        private int N;             // subtree count
+
+        public Node(Key key, Value val, boolean color, int N) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+            this.N = N;
+        }
+    }
+
+
+    /** 
+     * Adicionar nó em arvore RB
+    */
+
+    // insere o par key-value; sobrescreve valores se chave já existente
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+        assert check();
+    }
+    // recursivamente
+    private Node put(Node h, Key key, Value val) { 
+        if (h == null) return new Node(key, val, RED, 1);
+
+        int cmp = key.compareTo(h.key);
+        if      (cmp < 0) h.left  = put(h.left,  key, val); 
+        else if (cmp > 0) h.right = put(h.right, key, val); 
+        else              h.val   = val;
+
+        // fix-up any right-leaning links
+        if (isRed(h.right) && !isRed(h.left))      h = rotateLeft(h);
+        if (isRed(h.left)  &&  isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left)  &&  isRed(h.right))     flipColors(h);
+        h.N = size(h.left) + size(h.right) + 1;
+
+        return h;
+    }
+
+
+    //  METODOs auxiliares para o Nó
+
+    /*************************************************************************
+    *  Standard BST search
+    *************************************************************************/
+
+   /** Returns the value associated with the given key.
+    * If key is not in the symbol table, returns null.
+    */
+    public Value get(Key key) { 
+        return get(root, key); 
+    }
+
+    // returns the value associated with the given key 
+    // in the subtree rooted at x; null if no such key
+    // 
+    // devolve o valor associado com a chave key
+    // na subÃ¡rvore cuja raiz Ã© x; 
+    // devolve null se a key nÃ£o estÃ¡ na Ã¡rvore
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else              return x.val;
+        }
+        return null;
+    }
+
+   /** Does this symbol table contain a (key,value) pair 
+    * with the given key?
+    * <p>
+    * Esta tabela de sÃ­mbolos tem um par (chave,valor) cuja chave Ã© key?
+    */
+    public boolean contains(Key key) {
+        return (get(key) != null);
+    }
+
+    // is there a (key,value) pair with the given key 
+    // in the subtree rooted at x?
+    //
+    // hÃ¡ um par (chave,valor) com chave igual a key 
+    // na subÃ¡rvores cuja raiz Ã© x?
+    private boolean contains(Node x, Key key) {
+        return (get(x, key) != null);
+    }
+
+    // all of the keys, as an Iterable
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+    // the keys between lo and hi, as an Iterable
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<Key>();
+        // if (isEmpty() || lo.compareTo(hi) > 0) return queue;
+        keys(root, queue, lo, hi);
+        return queue;
+    } 
+    
+    // is node x red; false if x is null
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return (x.color == RED);
+    }
+
+
+ /*************************************************************************
+    *  Utility functions
+    *************************************************************************/
+
+    // height of tree (1-node tree has height 0)
+    public int height() { 
+        return height(root); 
+    }
+    private int height(Node x) {
+        if (x == null) return -1;
+        return 1 + Math.max(height(x.left), height(x.right));
+    }
+
+   /*************************************************************************
+    *  Ordered symbol table methods.
+    *************************************************************************/
+
+    // the smallest key; null if no such key
+    public Key min() {
+        if (isEmpty()) return null;
+        return min(root).key;
+    } 
+
+    // the smallest key in subtree rooted at x; null if no such key
+    private Node min(Node x) { 
+        assert x != null;
+        if (x.left == null) return x; 
+        else                return min(x.left); 
+    } 
+
+    // the largest key; null if no such key
+    public Key max() {
+        if (isEmpty()) return null;
+        return max(root).key;
+    } 
+
+    // the largest key in the subtree rooted at x; null if no such key
+    private Node max(Node x) { 
+        assert x != null;
+        if (x.right == null) return x; 
+        else                 return max(x.right); 
+    } 
+
+    // the largest key less than or equal to the given key
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        else           return x.key;
+    }    
+
+    // the largest key in the subtree rooted at x 
+    // less than or equal to the given key
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0)  return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t; 
+        else           return x;
+    }
+
+    // the smallest key greater than or equal to the given key
+    public Key ceiling(Key key) {  
+        Node x = ceiling(root, key);
+        if (x == null) return null;
+        else           return x.key;  
+    }
+
+    // the smallest key in the subtree rooted at x 
+    // greater than or equal to the given key
+    private Node ceiling(Node x, Key key) {  
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp > 0)  return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t != null) return t; 
+        else           return x;
+    }
+
+
+    // the key of rank k
+    public Key select(int k) {
+        if (k < 0 || k >= size())  return null;
+        Node x = select(root, k);
+        return x.key;
+    }
+
+    // the key of rank k in the subtree rooted at x
+    private Node select(Node x, int k) {
+        assert x != null;
+        assert k >= 0 && k < size(x);
+        int t = size(x.left); 
+        if      (t > k) return select(x.left,  k); 
+        else if (t < k) return select(x.right, k-t-1); 
+        else            return x; 
+    } 
+
+    // number of keys less than key
+    public int rank(Key key) {
+        return rank(key, root);
+    } 
+
+    // number of keys less than key in the subtree rooted at x
+    private int rank(Key key, Node x) {
+        if (x == null) return 0; 
+        int cmp = key.compareTo(x.key); 
+        if      (cmp < 0) return rank(key, x.left); 
+        else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right); 
+        else              return size(x.left); 
+    } 
+
+
+    //  Size methods
+
+    // number of nodes in subtree rooted at x; 
+    // 0 if x is null
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.N;
+    } 
+    /** Returns the number of (key,value) pairs in this symbol table.
+    * (i.e., the number of node os the tree).
+    */
+    public int size() { 
+        return size(root); 
+    }
+
+   /** Is this symbol table empty?<p>
+    * Esta tabela de sÃ­mbolos estÃ¡ vazia?
+    */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    // add the keys between lo and hi in the subtree rooted at x
+    // to the queue
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) { 
+        if (x == null) return; 
+        int cmplo = lo.compareTo(x.key); 
+        int cmphi = hi.compareTo(x.key); 
+        if (cmplo < 0) keys(x.left, queue, lo, hi); 
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key); 
+        if (cmphi > 0) keys(x.right, queue, lo, hi); 
+    }
+
+    // number keys between lo and hi
+    public int size(Key lo, Key hi) {
+        if (lo.compareTo(hi) > 0) return 0;
+        if (contains(hi)) return rank(hi) - rank(lo) + 1;
+        else              return rank(hi) - rank(lo);
+    }
+
+
+    /*************************************************************************
+    *  red-black tree helper functions
+    *************************************************************************/
+
+    // make a left-leaning link lean to the right
+    private Node rotateRight(Node h) {
+        assert (h != null) && isRed(h.left);
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = x.right.color;
+        x.right.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    // make a right-leaning link lean to the left
+    private Node rotateLeft(Node h) {
+        assert (h != null) && isRed(h.right);
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = x.left.color;
+        x.left.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    // flip the colors of a node and its two children
+    private void flipColors(Node h) {
+        // h must have opposite color of its two children
+        assert (h != null) && (h.left != null) && (h.right != null);
+        assert (!isRed(h) &&  isRed(h.left) &&  isRed(h.right))
+            || ( isRed(h) && !isRed(h.left) && !isRed(h.right));
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+
+    // Assuming that h is red and both h.left and h.left.left
+    // are black, make h.left or one of its children red.
+    private Node moveRedLeft(Node h) {
+        assert (h != null);
+        assert isRed(h) && !isRed(h.left) && !isRed(h.left.left);
+
+        flipColors(h);
+        if (isRed(h.right.left)) { 
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+        }
+        return h;
+    }
+
+    // Assuming that h is red and both h.right and h.right.left
+    // are black, make h.right or one of its children red.
+    private Node moveRedRight(Node h) {
+        assert (h != null);
+        assert isRed(h) && !isRed(h.right) && !isRed(h.right.left);
+        flipColors(h);
+        if (isRed(h.left.left)) { 
+            h = rotateRight(h);
+        }
+        return h;
+    }
+
+    // restore red-black tree invariant
+    private Node balance(Node h) {
+        assert (h != null);
+
+        if (isRed(h.right))                      h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))     flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+     /*************************************************************************
+    *  red-black checks
+    *************************************************************************/
+
+    private boolean check() {
+        if (!isBST())            StdOut.println("Not in symmetric order");
+        if (!isSizeConsistent()) StdOut.println("Subtree counts not consistent");
+        if (!isRankConsistent()) StdOut.println("Ranks not consistent");
+        if (!is23())             StdOut.println("Not a 2-3 tree");
+        if (!isBalanced())       StdOut.println("Not balanced");
+        return isBST() && isSizeConsistent() && isRankConsistent() 
+                       && is23() && isBalanced();
+    }
+
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) return true;
+        if (min != null && x.key.compareTo(min) <= 0) return false;
+        if (max != null && x.key.compareTo(max) >= 0) return false;
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    } 
+
+    private boolean isSizeConsistent() {
+        return isSizeConsistent(root); 
+    }
+    private boolean isSizeConsistent(Node x) {
+        if (x == null) return true;
+        if (x.N != size(x.left) + size(x.right) + 1) return false;
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    } 
+
+    private boolean isRankConsistent() {
+        for (int i = 0; i < size(); i++)
+            if (i != rank(select(i))) return false;
+        for (Key key : keys())
+            if (key.compareTo(select(rank(key))) != 0) return false;
+        return true;
+    }
+
+    // Does the tree have no red right links, and at most one (left)
+    // red links in a row on any path?
+    private boolean is23() { return is23(root); }
+    private boolean is23(Node x) {
+        if (x == null) return true;
+        if (isRed(x.right)) return false;
+        if (x != root && isRed(x) && isRed(x.left))
+            return false;
+        return is23(x.left) && is23(x.right);
+    } 
+     // equilibrio negro perfeito
+     private boolean isBalanced() { 
+        int black = 0;     // number of black links on path from root to min
+        Node x = root;
+        while (x != null) {
+            if (!isRed(x)) black++;
+            x = x.left;
+        }
+        return isBalanced(root, black);
+    }
+
+    // equilibrio negro perfeito 
+    // caminho da raiz até o link nullo tem mesma qntd ?
+    private boolean isBalanced(Node x, int black) {
+        if (x == null) return black == 0;
+        if (!isRed(x)) black--;
+        return isBalanced(x.left, black) && isBalanced(x.right, black);
+    } 
+
+
+
+    //main
+
+    public static void main(String[] args) { 
+        RedBlackBST<String, Integer> st = new RedBlackBST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+        for (String s : st.keys())
+            StdOut.println(s + " " + st.get(s));
+        StdOut.println();
+    }
+}
